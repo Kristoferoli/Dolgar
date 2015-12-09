a13b23aeb11aac396767588a17037725497421e8
Kristoferoli <kristoferb14@ru.is>
2015-12-09 17:52:16 +0000

    Update pythonskra.py

--- pythonskra.py
+++ pythonskra.py
@@ -1,14 +1,17 @@
 import psycopg2
 import getpass
 import random as r
+import pandas as pd
+
+
 
 
 host = 'localhost'
 #dbname = input('Database name: ')
-dbname = 'hlodver'
+dbname = 'hopaverkefni2'
 
 #username = input('User name for {}.{}: '.format(host,dbname))
-username = 'postgres'
+username = 'Krissi'
 #pw = getpass.getpass()
 pw = 1234
 
@@ -21,9 +24,11 @@ conn = psycopg2.connect(conn_string)
 cursor = conn.cursor()
 
 print("Connected!\n")
-thesqlcommands = open('movies.dat','r') #moguleiki ad tad turfi ad breyta slodinni
+thesqlcommands = open('/Users/Krissi/Desktop/HR/Gagnavinnsla/SQL/ml-100k/movies.dat','r') #moguleiki ad tad turfi ad breyta slodinni
 for i in thesqlcommands:
     index = int(i.split('::',1)[0])
-    Title = i.strip('::').split('::',2)[1]
+    #Title = i.strip('::').split('::',2)[1]
+    Title = i.split('::',1)[1].rsplit('(',1)[0].strip()
+    Year = i.rsplit(')',1)[0].rsplit('(',1)[1]
     Genre = i.split('::',2)[2].strip().split('|')
-    print([index], [Title], Genre)
+    print([index], [Title], [Year], Genre)
